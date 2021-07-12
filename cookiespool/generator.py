b4dd62380bad5be918f50b41ba367c04a7a11ec2
IvanLeezj <610776400@qq.com>
2021-07-12 11:47:48 +0800

    add cookiespool

---
+++ cookiespool/generator.py
@@ -0,0 +1,60 @@
+import json
+from cookiespool.config import *
+from cookiespool.db import RedisClient
+from login.login import WeiboCookies
+
+class CookiesGenerator():
+    def __init__(self, website='weibo'):
+        self.website = website
+        self.cookies_db = RedisClient('cookies', self.website)
+        self.accounts_db = RedisClient('accounts', self.website)
+
+    def process_cookies(self, cookies):
+        """
+        处理Cookies
+        :param cookies:
+        :return:
+        """
+        dict = {}
+        for cookie in cookies:
+            dict[cookie['name']] = cookie['value']
+        return dict
+
+    def run(self):
+        accounts_usernames = self.accounts_db.usernames()
+        cookies_usernames = self.cookies_db.usernames()
+
+        for username in accounts_usernames:
+            if not username in cookies_usernames:
+                password = self.accounts_db.get(username)
+                print('正在生成Cookies', '账号', username, '密码', password)
+                result = self.new_cookies(username, password)
+                if result.get('status') == 1:
+                    cookies = self.process_cookies(result.get('content'))
+                    print('成功获取到Cookies', cookies)
+                    if self.cookies_db.set(username, json.dumps(cookies)):
+                        print('成功保存Cookies')
+
+
+class WeiboCookiesGenerator(CookiesGenerator):
+    def __init__(self, website='weibo'):
+        """
+        初始化操作
+        :param website: 站点名称
+        :param browser: 使用的浏览器
+        """
+        CookiesGenerator.__init__(self, website)
+        self.website = website
+
+    def new_cookies(self, username, password):
+        """
+        生成Cookies
+        :param username: 用户名
+        :param password: 密码
+        :return: 用户名和Cookies
+        """
+        return WeiboCookies(username, password).run()
+
+if __name__ == '__main__':
+    test = WeiboCookiesGenerator()
+    test.run()
---
+++ cookiespool/importer.py
@@ -0,0 +1,25 @@
+"""
+录入微博账号和密码到redis
+"""
+from cookiespool.db import RedisClient
+
+conn = RedisClient('accounts', 'weibo')
+
+def set(account, sep='----'):
+    username, password = account.split(sep)
+    result = conn.set(username, password)
+    print('账号', username, '密码', password)
+    print('录入成功' if result else '录入失败')
+
+
+def scan():
+    print('请输入账号密码组(格式：账号----密码) 输入exit退出读入')
+    while True:
+        account = input()
+        if account == 'exit':
+            break
+        set(account)
+
+
+if __name__ == '__main__':
+    scan()
